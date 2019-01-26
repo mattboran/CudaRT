70a26481b16d4f23795a2c614190547fe0572cbb
mattboran <mattboran@gmail.com>
2019-01-25 19:36:01 -0500

    Started to add LensedCamera

--- src/include/camera.h
+++ src/include/camera.h
@@ -1,11 +1,11 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <cuda_runtime.h>
 #include "geometry.h"
 
 struct Sampler;
-class Camera
-{
+class Camera {
 public:
 	__host__ Camera() {}
 	__host__ Camera(std::string filename, int width, int height);
@@ -14,7 +14,7 @@ public:
 	__host__ void rebase();
 
 	__host__ __device__ Ray computeCameraRay(int i, int j, Sampler* p_sampler) const;
-private:
+protected:
 	Vector3Df eye;
 	Vector3Df dir;
 	Vector3Df up;
@@ -23,4 +23,14 @@ private:
 	int xpixels, ypixels;
 };
 
+class LensedCamera : public Camera {
+public:
+	__host__ LensedCamera() : Camera() {}
+	__host__ LensedCamera(std::string filename, int width, int height) :
+			Camera(filename, width, height) {}
+	__host__ LensedCamera(Vector3Df pos, Vector3Df target, Vector3Df upv, Vector3Df rt, float _fov, int x, int y) :
+		Camera(pos, target, upv, rt, _fov, x, y) {}
+private:
+	float focalDistance, apertureWidth;
+};
 #endif
